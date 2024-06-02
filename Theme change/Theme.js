f689d97d046127ca48dd37975ccdf74b173286db
shubham shinde <shubhamshinde8412@gmail.com>
2024-06-02 13:12:02 +0530

    add theme project

---
+++ Theme change/Theme.js
@@ -0,0 +1,23 @@
+ const btn = document.querySelector("#btn");
+const body = document.querySelector("body");
+const para = document.querySelector("#para");
+let currmode="light";
+ btn.addEventListener("click",()=>{
+if(currmode==="light"){
+currmode="dark";
+body.classList.add("dark");
+body.classList.remove("light");
+para.innerText="this is a dark theme".toUpperCase();
+btn.style.color="white";
+btn.style.backgroundColor="pink";
+}
+else{
+    currmode="light";
+    body.classList.add("light");
+    body.classList.remove("dark");
+    para.innerText="this is a light theme".toUpperCase();
+    btn.style.color="red";
+    btn.style.backgroundColor="aqua";
+    
+}
+ })
