89843773f7aede5ca2b78aae9673dcdfff8c997b
shubham shinde <shubhamshinde8412@gmail.com>
2024-07-19 17:54:30 +0530

    add portfolio Website

--- 2-bharat-clock-version-one/src/components/CurrentTime.jsx
+++ 2-bharat-clock-version-one/src/components/CurrentTime.jsx
@@ -1,5 +1,17 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 let CurrentTime = () => {
-  const date = new Date();
+  const [date, setdate] = useState(new Date());
+  useEffect(() => {
+    const intertime = setInterval(() => {
+      setdate(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(intertime);
+    };
+  }, []);
+
   return (
     <p className="lead">
       This is the current time: {date.toLocaleDateString()} -{" "}
